f91f943fce0dbc50ae1daa38323e63058840f8fd
Dmitrij Biriukov <dmb@danskebank.lt>
2020-04-06 16:44:56 +0300

    Fix broken npm test with newest libs for enzyme and RJS16

--- src/components/ProgressBar/ProgressBar.spec.js
+++ src/components/ProgressBar/ProgressBar.spec.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, configure } from "enzyme";
 import ProgressBar from "./ProgressBar";
+import Adapter from "enzyme-adapter-react-16";
 
+configure({ adapter: new Adapter() });
 describe("ProgressBar", () => {
   test("getWidthAsPercentOfTotalWidth should return 250 with widht and 50 %", () => {
     const wrapper = shallow(<ProgressBar percent={50} width={500} />);
